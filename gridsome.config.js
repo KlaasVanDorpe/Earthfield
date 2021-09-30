cda991c891b0c4139056f4683729ae36c8892ff3
Klaas Van Dorpe <klaas.van.dorpe@outlook.be>
2021-09-30 23:30:25 +0200

    barebonnes

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Earthfield',
+  plugins: [],
+  port: 8090,
 }
